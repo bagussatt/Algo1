b7f8dec25608bc4966b54ea60fd52a085a3d5756
bagussatt <bagussam879@gmail.com>
2022-10-07 17:46:18 +0700

    DD

--- Algo1/Program.cs
+++ Algo1/Program.cs
@@ -6,10 +6,65 @@ using System.Threading.Tasks;
 
 namespace Algo1
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+
+        private int[] a = new int[20];
+        private int n;
+
+        public void read()
         {
+            while (true)
+            {
+                Console.Write("Masukkan banyaknya elemen pada Array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 20 elemen.\n");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine(" Masukan elemen array ");
+            Console.WriteLine("-----------------------");
+            for (int i = 0; i < n; i++) 
+            {
+                Console.WriteLine("<" + (i + 1) + "> ");
+                String s1 = Console.ReadLine();
+                a[i] = Int32.Parse(s1);
+            }
+        }
+        public void display ()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine(" Element array yang telah tersusun ");
+            Console.WriteLine("-----------------------------------");
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(a[j]);
+            }
+            Console.WriteLine("");
+        }
+        public void bubbleShortArray()
+        {
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 0; j < n - i ; j++)
+                {
+                    if (a[j] > a[j + 1])
+
+                    {
+                        int temp;
+                        temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+         
+            // gabisa committ
         }
     }
 }
